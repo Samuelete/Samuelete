bda12481932173c851210330c71ed74131236a71
Samuelete <89805091+Samuelete@users.noreply.github.com>
2021-09-04 09:12:18 -0500

    last update

--- lab5/buggy4.js
+++ lab5/buggy4.js
@@ -1,11 +1,11 @@
-/*function somethingElse() {
+function somethingElse() {
     if (5 > 2) {
         alert("Bigger");
-    //} // Bug here It was not } in this line and it was making the error
+    } // Bug here It was not } in this line and it was making the error
     if (5 < 2) {
         alert("Smaller");
     }
-}*/
+}
 
 function gogogo() {
     alert("Yay, it works!");
