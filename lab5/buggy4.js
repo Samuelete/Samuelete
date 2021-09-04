77ef6501a0f2b2b828891831b3d01f8c290d1467
Samuelete <89805091+Samuelete@users.noreply.github.com>
2021-09-04 09:08:45 -0500

    update saturday

---
+++ lab5/buggy4.js
@@ -0,0 +1,12 @@
+/*function somethingElse() {
+    if (5 > 2) {
+        alert("Bigger");
+    //} // Bug here It was not } in this line and it was making the error
+    if (5 < 2) {
+        alert("Smaller");
+    }
+}*/
+
+function gogogo() {
+    alert("Yay, it works!");
+}
