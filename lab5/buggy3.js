e9dcea37a7fc0e4a8f99d61a1a52781d7d76e129
Samuelete <89805091+Samuelete@users.noreply.github.com>
2021-09-03 20:10:07 -0500

    Friday Update

---
+++ lab5/buggy3.js
@@ -0,0 +1,5 @@
+function gogogo() { // Bug found it here. It should have ; after gogogo()
+    alert("Yay, it works!"); 
+    // Bug found here It has '' instead of "
+    // also the function alert() started with A instead of a
+}
